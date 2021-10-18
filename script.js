87a521a35168b5c50d69d613c02b7183c1ea5fd2
dezoliveira <andresoliveira@protonmail.com>
2021-10-18 18:13:13 -0300

    Pokedex - Start

---
+++ script.js
@@ -0,0 +1,42 @@
+const pokedex = document.getElementById("pokedex")
+
+console.log(pokedex)
+
+
+const fetchPokemon = () => {
+	
+	const promises = []
+
+	for(let i = 1; i <= 150; i++){
+		const url = `https://pokeapi.co/api/v2/pokemon/${i}`
+		promises.push(fetch(url).then((res) => res.json()))
+	}
+
+	Promise.all(promises).then(results => {
+		const pokemon = results.map(data => ({
+			name: data.name,
+			id: data.id,
+			image: data.sprites['front_default'],
+			type: data.types.map((type) => type.type.name).join(", ")
+		}))
+
+		displayPokemon(pokemon)
+	})
+}
+
+const displayPokemon = (pokemon) => {
+	console.log(pokemon)
+	const pokemonHTMLString = pokemon.map (pokemons  => `
+	<li>
+		<img src="${pokemons.image}"/>
+		<h2>${pokemons.id}. ${pokemons.name}</h2>
+		<p>Type: ${pokemons.type}</p>
+	</li>
+	`
+	).join('')
+	pokedex.innerHTML = pokemonHTMLString;
+}
+
+
+fetchPokemon()
+
